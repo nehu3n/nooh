b65c011b8996829b3e9d43002be71813f474260e
nehu3n <developer.nh3@gmail.com>
2026-09-12 01:52:50 -0300

    feat(compiler): compose code generation

---
+++ packages/compiler/src/generate.ts
@@ -0,0 +1,42 @@
+import { generateAppModule } from "@/generate-app";
+import { generateGroupModule } from "@/generate-group";
+import { generateRouterModule } from "@/generate-router";
+import { generateTypesModule } from "@/generate-types";
+
+import type {
+  CompilationPlan,
+  GeneratedModule,
+  GeneratedOutput,
+  ProjectModel,
+} from "@/types";
+
+export const generate = (
+  plan: CompilationPlan,
+  model: ProjectModel
+): GeneratedOutput => {
+  const modules: GeneratedModule[] = [];
+
+  const typesModule = generateTypesModule(plan, model.config);
+
+  modules.push(typesModule);
+
+  const routerPaths = [
+    ...new Set(model.routes.map((route) => route.routerPath)),
+  ].sort();
+
+  for (const routerPath of routerPaths) {
+    modules.push(generateRouterModule(plan, model, routerPath));
+  }
+
+  for (const group of model.groups) {
+    modules.push(generateGroupModule(plan, model, group));
+  }
+
+  modules.push(generateAppModule(plan, model));
+
+  modules.sort((a, b) => a.id.localeCompare(b.id));
+
+  return {
+    modules,
+  };
+};
